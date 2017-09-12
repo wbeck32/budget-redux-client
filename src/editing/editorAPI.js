855a0be7a8dcb777d88b6d12e53d375b1efed5a1
Wendy Beck <webeck@gmail.com>
2017-09-11 17:13:07 -0700

    able to save a category to a user

--- src/editing/editorAPI.js
+++ src/editing/editorAPI.js
@@ -6,10 +6,8 @@ const ME_API_URL = process.env.REACT_APP_ME_API_URL;
 export default {
   createNewCat : async ({payload}) => {
     const {name, catAmount, catRemaining} = payload;
-    // console.log('payload in newcat api: ', payload)
-    const newCat = await req.post(`${ME_API_URL}/category`)
+    return await req.post(`${ME_API_URL}/category`)
     .set('Authorization', store.getState().token)
     .send({name: name, catAmount: catAmount, catRemaining: catRemaining});
-    console.log(newCat);
   }
 }
--- src/helpers/verify.js
+++
@@ -1,8 +0,0 @@
-import authAPI from '../auth/authAPI';
-
-export default async function verifyUser() {
-  const verified = await authAPI.verify();
-  console.log('verified: ', verified)
-  return verified;
-
-}
